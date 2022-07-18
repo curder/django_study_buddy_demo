ee5045970cf82eb462e2131fcd72c7394282eaa7
curder <q.curder@gmail.com>
2022-07-18 14:30:37 +0800

    dynamic route params

--- base/views.py
+++ base/views.py
@@ -9,8 +9,16 @@ rooms = [
 
 def home(request):
     context = {'rooms': rooms}
-    return render(request, 'home.html', context=context)
+    return render(request, 'base/home.html', context=context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {"room": room}
+
+    return render(request, 'base/room.html', context=context)
