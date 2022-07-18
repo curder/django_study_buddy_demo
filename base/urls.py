189abb9f95a61fe5c23310b298946bb5b471259d
curder <q.curder@gmail.com>
2022-07-18 16:37:12 +0800

    destroy room

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('room/<str:pk>/destroy/', views.destroyRoom, name='rooms.destroy'),
     path('room/<str:pk>/update/', views.updateRoom, name='rooms.update'),
     path('room/create/', views.createRoom, name='rooms.create'),
     path('room/<str:pk>', views.room, name='room'),
