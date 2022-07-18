f2b2ecfcf39924f4544cf3963256e7c629f2636f
curder <q.curder@gmail.com>
2022-07-18 14:21:50 +0800

    assign variables from view to template

--- base/views.py
+++ base/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+rooms = [
+    {"id": 1, "name": "Let's learn Python!"},
+    {"id": 2, "name": "Design with me"},
+    {"id": 3, "name": "Frontend developers"},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context=context)
 
 
-def rooms(request):
-    return render(request, 'rooms.html')
+def room(request):
+    return render(request, 'room.html')
