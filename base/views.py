2395e93e9143fb8e9cfa23c6c14892d1432c138e
curder <q.curder@gmail.com>
2022-07-18 16:53:09 +0800

    filter by topic

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -11,8 +11,10 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()  # 获取所有表数据
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()  # 获取所有表数据
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context=context)
 
 
