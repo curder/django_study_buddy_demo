c1ef0ad914d1f55171c8fc00765edab46f20331a
curder <q.curder@gmail.com>
2022-07-18 16:20:55 +0800

    update room

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/create', views.createRoom, name='rooms.create'),
+    path('room/<str:pk>/update/', views.updateRoom, name='rooms.update'),
+    path('room/create/', views.createRoom, name='rooms.create'),
     path('room/<str:pk>', views.room, name='room'),
 ]
