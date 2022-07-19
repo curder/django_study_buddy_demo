cddda46393a0f08c7f162e81dacbf7b433523504
curder <q.curder@gmail.com>
2022-07-19 11:16:09 +0800

    recent activity

--- base/models.py
+++ base/models.py
@@ -32,5 +32,8 @@ class Message(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-updated_at', '-created_at']
+
     def __str__(self):
-        return self.body[:50]
+        return self.body[0:50]
