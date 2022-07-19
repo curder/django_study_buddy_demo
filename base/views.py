7f3e4c15fb547cd99bfa4243274f251fc0de3fde
curder <q.curder@gmail.com>
2022-07-19 10:03:06 +0800

    Conversation and Comments

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from django.contrib.auth.models import User
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -81,7 +81,13 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {"room": room}
+    room_messages = room.message_set.all().order_by('-created_at')
+
+    if request.method == 'POST':
+        message = Message.objects.create(user=request.user, room=room, body=request.POST.get('body'))
+        return redirect('room', pk=room.id)
+
+    context = {"room": room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context=context)
 
 
